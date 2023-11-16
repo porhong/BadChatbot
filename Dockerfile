9e93501573f8982082a8278e70d76cb7fc328079
porhong <poehongkeat@gmail.com>
2023-11-16 13:29:30 +0700

    V1.2.0(Beta)

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,10 @@
 FROM python:3.9.18-slim
-ADD gpt.py .
-ADD keep_alive.py .
+ADD main_1.2.0.py .
 ADD .env .
 RUN apt update
+RUN pip 
 RUN pip install --upgrade openai
 RUN pip install aiogram==2.25.2
-RUN pip install pillow
 RUN pip install requests
-RUN pip install flask
 RUN pip install python-dotenv
 CMD [ "python3","./gpt.py" ]
--- main_1.2.0.py
+++ main_1.2.0.py
@@ -3,9 +3,6 @@ from aiogram import Bot, Dispatcher, types, executor
 
 
 API_KEY = 'AIzaSyBcJCFrUHkLrF5ZhjJ_dsGyitgIPvqZlMc'
-
-old_msg = []
-
 bot = Bot(token="6538080926:AAFMORmrgG5Bh0Ri7ng4MX8HJ47lhxVGajE")
 dp = Dispatcher(bot)
 genai.configure(api_key='AIzaSyBcJCFrUHkLrF5ZhjJ_dsGyitgIPvqZlMc')
@@ -21,7 +18,7 @@ async def welcome(message: types.Message):
     await message.reply('''Bot Cleaned!! 🧹''')
 
 
-response = ""
+
 
 
 def start_chat(text, type):
@@ -29,10 +26,18 @@ def start_chat(text, type):
     if type == 1:
         response = genai.chat(
             model='models/chat-bison-001', temperature=0.50, top_p=0.95, top_k=40, messages=[text])
-        return response.last
-    else:
+        if response.last is None:
+            answer = "I can not answer for this topic 😥"
+        else:
+            answer = response.last
+        return answer
+    elif type == 2:
         response = response.reply(text)
-        return response.last
+        if response.last is None:
+            answer = "I can not answer for this topic 😥"
+        else:
+            answer = response.last
+        return answer
 
 
 @dp.message_handler()
@@ -40,13 +45,20 @@ async def chat(message: types.Message):
     try:
         text = message.text
         if message.reply_to_message is None:
+            await message.answer_chat_action("typing")
             answer = start_chat(text, 1)
+            await message.answer_chat_action("typing")
             await message.reply(answer)
         else:
+            await message.answer_chat_action("typing")
             answer = start_chat(text, 2)
+            await message.answer_chat_action("typing")
             await message.reply(answer)
     except Exception as e:
         print(e)
+        await message.reply(f"""Error⚠️
+Bot was ran into an errot: <b>{e}</b>
+Please report this case to administrator.""")
 
 
 async def main() -> None:
@@ -54,4 +66,5 @@ async def main() -> None:
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
+    print("Service Started🟢")
     executor.start_polling(dp)
